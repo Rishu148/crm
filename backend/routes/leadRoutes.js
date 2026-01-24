4f95539577c4ca97a324cc242e8a7fc9604b06c3
Rishu148 <dhakedroopkishor@gmail.com>
2026-01-24 21:11:47 +0530

    "Fix case sensitivity import path"

--- backend/routes/leadRoutes.js
+++ backend/routes/leadRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const { createLead, getLeads, bulkUpload, downloadTemplate, getAgents, assignLeads,updateLead, deleteLead,deleteLeadsBulk} = require("../controllers/leadController");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../Middleware/authMiddleware");
 
 // Memory Storage for Excel Processing
 const storage = multer.memoryStorage();
