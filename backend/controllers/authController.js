409a5ac2bf4f0ba4077eab22a6f6d6a6a391c570
Rishu148 <dhakedroopkishor@gmail.com>
2026-01-27 20:04:42 +0530

    fix cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,8 +15,8 @@ const sendToken = (user, statusCode, res) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000, // 1 Day
   });
 
